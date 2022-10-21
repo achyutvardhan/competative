9df77076b2069cac1d268d74dc9224a821cf31d7
achyut vardhan <achyutvardhan234@gmail.com>
2022-10-21 21:03:20 +0530

    updated coffee

--- internship/coffee.cpp
+++ internship/coffee.cpp
@@ -2,51 +2,143 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-class coffee
+class Coffee
 {
-private:
-    int espresso[3];
-    int capp[3];
-    int Latte[3];
-    int n;
+protected:
+    int milk;
+    int cream;
+    int latte;
+    string name;
      int total_bill;
+     int addOn[3];
 public:
-    coffee()
+    Coffee()
     {
         total_bill=0;
-        n = 3;
-        espresso[0]=60;
-        espresso[1]=75;
-        espresso[2]=100;
-        capp[0]=80;
-        capp[1]=90;
-        capp[2]=125;
-        Latte[0]=100;
-        Latte[1]=125;
-        Latte[2]=150;
-
+        addOn[0]=addOn[2]=addOn[1] = 0;
     }
-    void customer();
     void display_list();
+    void add_on(){
+        int val=0;
+        cout<<"enter your choice"<<endl;
+        while (val!=4)
+        {
+            display_list();
+            cin>>val;
+            switch (val)
+            {
+            case 1:
+                addOn[0]++;
+                total_bill+=milk;
+                break;
+            case 2:
+                addOn[1]++;
+                total_bill+=cream;
+                break;
+            case 3:
+                addOn[2]++;
+                total_bill+=latte;
+                break;
+            case 4:
+                break;
+            default:
+            cout<<"invalid input";
+                break;
+            }
+        }
+        
+    }
 
 };
+void Coffee::display_list(){
+      cout<< "Product/Add-on"<<"\t"<<"1. Milk"<<"\t"<<"2. Cream"<<"\t"<<"3. Latte"<<endl;
+      cout<<name<<"\t"<<milk<<"\t"<< cream<< "\t"<< latte<<endl;
+}
 
-void coffee::customer(){
-    display_list();
-    cout<<"please place your order:";
-    int a;
+class Expresso : public Coffee
+{
+private:
+  
+public:
+    Expresso (){
+       milk = 60;
+       cream = 75;
+       latte = 100;
+       name  = "Espresso";
+    }
+   
+
+};
+class Cappuccino : public Coffee
+{
+private:
+  
+public:
+    Cappuccino (){
+       milk = 80;
+       cream = 90;
+       latte = 125;
+       name  = "Cappuccino";
+    }
+  
+};
+
+class Latte : public Coffee
+{
+private:
+  
+public:
+    Latte (){
+       milk = 100;
+       cream = 125;
+       latte = 150;
+       name = "Latte";
+    }
+   
     
+};
+
+void bill(vector<Coffee> &l){
+
 }
-void coffee::display_list(){
-   cout<< "Product/Add-on"<<"\t"<<"Milk"<<"\t"<<"Cream"<<"\t"<<"Latte"<<endl;
-   cout<<"Espresso"<<"\t"<<espresso[0]<<"\t"<< espresso[1]<< "\t"<< espresso[2]<<endl;
-   cout<<"cappuccino"<<"\t"<<capp[0]<<"\t"<< capp[1]<< "\t"<< capp[2]<<endl;
-   cout<<"Latte"<<"\t\t"<<Latte[0]<<"\t"<< Latte[1]<< "\t"<< Latte[2]<<endl;
-}
+
+ void select_coffee(){
+       int a=0;
+       cout<<"1. Expresso\n"<<"2. Cappuccino\n"<<"3. Latte\n"<<"4.Exit\n"<<"enter your choice"<<endl;
+       vector<Coffee> l;
+      while (a!=4)
+      {
+        cin>>a;
+        switch (a)
+        {
+        case 1:{ Expresso obj; 
+        obj.add_on();
+        }
+            break;
+        case 2:{ Cappuccino obj; 
+        obj.add_on();
+        }
+            break;
+        case 3:{ Latte obj; 
+        obj.add_on();
+        }
+            break;
+        case 4:
+        bill(l);
+        exit(0);
+        break;
+        
+        default:
+        cout<<"invalid input";
+            break;
+        }
+      }
+      
+    }
 
 int main()
 {
-    coffee c;
+    Coffee c;
     c.display_list();
     return 0;
 }
