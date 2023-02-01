e8291bc90a060bd8f57e06ae4778968391b5bca2
achyut vardhan <achyutvardhan234@gmail.com>
2023-02-01 21:34:29 +0530

    1/2/23

---
+++ Redistributing_Chocolates.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+#define f(i, a, b) for (ll i = a; i < b; i++)
+#define r(i, a, b) for (ll i = a; i > b; i--)
+#define vll vector<ll>
+#define mll map<ll, ll>
+#define pll pair<ll, ll>
+#define mp map<pll, ll>
+///.........Bit_Manipulation...........///
+#define MSB(mask) 63 - __builtin_clzll(mask) /// 0 -> -1
+#define LSB(mask) __builtin_ctzll(mask)      /// 0 -> 64
+#define SETBIT(mask) __builtin_popcountll(mask)
+#define CHECKBIT(mask, bit) (mask & (1LL << bit))
+#define ONBIT(mask, bit) (mask | (1LL << bit))
+#define OFFBIT(mask, bit) (mask & ~(1LL << bit))
+#define CHANGEBIT(mask, bit) (mask ^ (1LL << bit))
+
+void solve()
+{
+    int a, b, c;
+    cin >> a >> b >> c;
+    if (a == 0 || b == 0 || c == 0)
+        cout << "NO" << endl;
+    else if (a == 1 && b == 1 && c == 1)
+        cout << "NO" << endl;
+    else if (a == b && a != c && (abs(a - c) == 1 || abs(a - c) == 2))
+        cout << "NO" << endl;
+    else if (a == c && a != b && (abs(a - b) == 1 || abs(a - b) == 2))
+        cout << "NO" << endl;
+    else if (b == c && a != b && (abs(a - b) == 1 || abs(a - b) == 2))
+        cout << "NO" << endl;
+    else
+        cout << "YES" << endl;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0), cout.tie(0);
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+}
