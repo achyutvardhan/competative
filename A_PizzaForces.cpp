f5277f512212f951e919205e2f3ff81c487dbacf
achyut vardhan <achyutvardhan234@gmail.com>
2023-02-23 17:44:48 +0530

    23/2/23

---
+++ A_PizzaForces.cpp
@@ -0,0 +1,60 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+#define f(i, a, b) for (ll i = a; i < b; i++)
+#define r(i, a, b) for (ll i = a; i > b; i--)
+#define vll vector<ll>
+#define mll map<ll, ll>
+#define pll pair<ll, ll>
+#define mp map<pll, ll>
+///.........Bit_Manipulation...........///
+#define MSB(mask) 63 - __builtin_clzll(mask) /// 0 -> -1
+#define LSB(mask) __builtin_ctzll(mask)      /// 0 -> 64
+#define SETBIT(mask) __builtin_popcountll(mask)
+#define CHECKBIT(mask, bit) (mask & (1LL << bit))
+#define ONBIT(mask, bit) (mask | (1LL << bit))
+#define OFFBIT(mask, bit) (mask & ~(1LL << bit))
+#define CHANGEBIT(mask, bit) (mask ^ (1LL << bit))
+
+void solve()
+{
+    ll n;
+    cin >> n;
+    ll pizza = n / 6;
+    ll remain = n % 6;
+    if (n<6)
+    {
+        cout<<15<<endl;
+        return;
+    }
+    
+    if (remain == 1 || remain == 2)
+    {
+        cout << ((pizza - 1) * 15) + 20 << endl;
+    }
+    else if (remain == 3 || remain == 4)
+    {
+        cout << ((pizza - 1) * 15) + 25 << endl;
+        ;
+    }
+    else if (remain == 0)
+    {
+        cout << pizza * 15 << endl;
+    }
+    else
+    {
+        cout << (pizza + 1) * 15 << endl;
+    }
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0), cout.tie(0);
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+}
