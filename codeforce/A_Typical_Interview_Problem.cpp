36c7cc90d82a6dbc2b08d7cf3b9582fe088754ce
achyut vardhan <achyutvardhan234@gmail.com>
2023-03-01 00:34:26 +0530

    18/2/23

---
+++ codeforce/A_Typical_Interview_Problem.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int  
+#define f(i,a,b) for (ll i = a; i < b; i++)
+#define r(i,a,b) for (ll i = a; i > b; i--)
+#define vll vector<ll>
+#define mll map<ll,ll>
+#define pll pair<ll,ll>
+#define mp map<pll,ll>
+///.........Bit_Manipulation...........///
+#define MSB(mask) 63-__builtin_clzll(mask)  /// 0 -> -1
+#define LSB(mask) __builtin_ctzll(mask)  /// 0 -> 64
+#define SETBIT(mask) __builtin_popcountll(mask)
+#define CHECKBIT(mask,bit) (mask&(1LL<<bit))
+#define ONBIT(mask,bit) (mask|(1LL<<bit))
+#define OFFBIT(mask,bit) (mask&~(1LL<<bit))
+#define CHANGEBIT(mask,bit) (mask^(1LL<<bit))
+ 
+ 
+void solve(){
+    ll n;
+    cin>>n;
+    string s;
+    cin>>s;
+    string patt = "FBFFBFFBFBFFBFFBFBFFBFFBFBFFBFFB";
+    ll index_str =  patt.find(s);
+    if (index_str!= string::npos)
+    {
+        cout<<"YES"<<endl;
+    }else{
+        cout<<"NO"<<endl;
+    }
+    
+    
+    
+}
+ 
+ 
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0),cout.tie(0);
+     int t;
+     cin>>t;
+  while(t--){
+     solve();
+    }
+}
