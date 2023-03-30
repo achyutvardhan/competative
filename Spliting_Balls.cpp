9835d9b4d868b7ec6abc1bbbc77a32d6c0c18571
achyut vardhan <achyutvardhan234@gmail.com>
2023-03-30 23:44:32 +0530

    30/3/23

---
+++ Spliting_Balls.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+#define f(i, a, b) for (ll i = a; i < b; i++)
+#define r(i, a, b) for (ll i = a; i > b; i--)
+#define vll vector<ll>
+#define mll map<ll, ll>
+#define pll pair<ll, ll>
+#define mp map<pll, ll>
+///.........Bit_Manipulation...........///
+#define MSB(mask) 63 - __builtin_clzll(mask) /// 0 -> -1
+#define LSB(mask) __builtin_ctzll(mask)      /// 0 -> 64
+#define SETBIT(mask) __builtin_popcountll(mask)
+#define CHECKBIT(mask, bit) (mask & (1LL << bit))
+#define ONBIT(mask, bit) (mask | (1LL << bit))
+#define OFFBIT(mask, bit) (mask & ~(1LL << bit))
+#define CHANGEBIT(mask, bit) (mask ^ (1LL << bit))
+
+void solve()
+{
+    ll n;
+    cin >> n;
+    const int M = 1000000007;
+    vll v(n);
+    vll temp(1000000);
+    f(i, 0, n)
+            cin >> v[i];
+    ll ans = 0;
+    temp[0] = 1;
+    f(i, 1, 1000000)
+    {
+        temp[i] = (temp[i - 1] * i) % M;
+    }
+    f(i, 0, n)
+    {
+        ans += temp[v[i]];
+    }
+    cout << ans % M << endl;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0), cout.tie(0);
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+}
