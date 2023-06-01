68f433328e637ef213a472ab1e212ef4535d3cd5
achyut vardhan <achyutvardhan234@gmail.com>
2023-06-01 13:22:53 +0530

    1/06/23

---
+++ codeforce/A_LuoTianyi_and_the_Palindrome_String.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int  
+#define f(i,a,b) for (ll i = a; i < b; i++)
+#define r(i,a,b) for (ll i = a; i > b; i--)
+#define vll vector<ll>
+#define mll map<ll,ll>
+#define pll pair<ll,ll>
+#define mp map<pll,ll>
+///.........Bit_Manipulation...........///
+#define MSB(mask) 63-__builtin_clzll(mask)  /// 0 -> -1
+#define LSB(mask) __builtin_ctzll(mask)  /// 0 -> 64
+#define SETBIT(mask) __builtin_popcountll(mask)
+#define CHECKBIT(mask,bit) (mask&(1LL<<bit))
+#define ONBIT(mask,bit) (mask|(1LL<<bit))
+#define OFFBIT(mask,bit) (mask&~(1LL<<bit))
+#define CHANGEBIT(mask,bit) (mask^(1LL<<bit))
+ 
+ 
+void solve(){
+    string s;
+    cin>>s;
+    ll count =0 ;
+    for (auto i = 1; i <s.size(); i++)
+        if (s[i]==s[i-1])
+            count++;
+        
+    if (s.size()==(count+1))
+    {
+       cout<<-1<<endl;
+    }
+    else{
+        cout<<s.size()-1<<endl;
+    }
+    
+}
+ 
+ 
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0),cout.tie(0);
+     int t;
+     cin>>t;
+  while(t--){
+     solve();
+    }
+}
