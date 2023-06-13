a271beeb36013b760b84bf276f476f7831693dc4
achyut vardhan <achyutvardhan234@gmail.com>
2023-06-13 14:18:26 +0530

    13/6/23

---
+++ codeforce/A_Insert_Digit.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int  
+#define f(i,a,b) for (ll i = a; i < b; i++)
+#define r(i,a,b) for (ll i = a; i > b; i--)
+#define vll vector<ll>
+#define mll map<ll,ll>
+#define pll pair<ll,ll>
+#define mp map<pll,ll>
+///.........Bit_Manipulation...........///
+#define MSB(mask) 63-__builtin_clzll(mask)  /// 0 -> -1
+#define LSB(mask) __builtin_ctzll(mask)  /// 0 -> 64
+#define SETBIT(mask) __builtin_popcountll(mask)
+#define CHECKBIT(mask,bit) (mask&(1LL<<bit))
+#define ONBIT(mask,bit) (mask|(1LL<<bit))
+#define OFFBIT(mask,bit) (mask&~(1LL<<bit))
+#define CHANGEBIT(mask,bit) (mask^(1LL<<bit))
+ 
+ 
+void solve(){
+    ll n,d;
+    cin>>n>>d;
+    string s;
+    cin>>s;
+    int count =0;
+    f(i,0,n)
+    {
+        ll digit = s[i]-'0';
+        if(digit<d)
+        {
+          s.insert(i,to_string(d));
+          count++;
+            break;
+        }
+    }
+
+    if(count==0)
+         s.insert(n,to_string(d));
+
+    
+    cout<<s<<endl;
+}
+ 
+ 
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0),cout.tie(0);
+     int t;
+     cin>>t;
+  while(t--){
+     solve();
+    }
+}
