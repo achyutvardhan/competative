59eb2945ff7c481a3c0021571790550b02f38a08
achyut vardhan <achyutvardhan234@gmail.com>
2023-06-16 20:24:42 +0530

    16/6/23

---
+++ codeforce/B_The_String_Has_a_Target.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+#define f(i, a, b) for (ll i = a; i < b; i++)
+#define r(i, a, b) for (ll i = a; i > b; i--)
+#define vll vector<ll>
+#define mll map<ll, ll>
+#define pll pair<ll, ll>
+#define mp map<pll, ll>
+///.........Bit_Manipulation...........///
+#define MSB(mask) 63 - __builtin_clzll(mask) /// 0 -> -1
+#define LSB(mask) __builtin_ctzll(mask)      /// 0 -> 64
+#define SETBIT(mask) __builtin_popcountll(mask)
+#define CHECKBIT(mask, bit) (mask & (1LL << bit))
+#define ONBIT(mask, bit) (mask | (1LL << bit))
+#define OFFBIT(mask, bit) (mask & ~(1LL << bit))
+#define CHANGEBIT(mask, bit) (mask ^ (1LL << bit))
+
+void solve()
+{
+    ll n;
+    cin >> n;
+    string s;
+    cin >> s;
+    char ch = s[0];
+    ll loc = 0;
+    f(i, 0, n)
+    {
+        if (ch >= s[i])
+        {
+            ch = s[i];
+            loc = i;
+        }
+    }
+    
+    cout<<ch;
+    f(i,0,loc)cout<<s[i];
+    f(i,loc+1,n)cout<<s[i];
+    cout<<endl;
+}
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0), cout.tie(0);
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+}
