adcc3ca0ea5fd914d66b4f38db080a1a48884929
achyut vardhan <achyutvardhan234@gmail.com>
2023-06-23 16:57:23 +0530

    23/6/23

---
+++ codeforce/B_Taisia_and_Dice.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+#define f(i, a, b) for (ll i = a; i < b; i++)
+#define r(i, a, b) for (ll i = a; i > b; i--)
+#define vll vector<ll>
+#define mll map<ll, ll>
+#define pll pair<ll, ll>
+#define mp map<pll, ll>
+///.........Bit_Manipulation...........///
+#define MSB(mask) 63 - __builtin_clzll(mask) /// 0 -> -1
+#define LSB(mask) __builtin_ctzll(mask)      /// 0 -> 64
+#define SETBIT(mask) __builtin_popcountll(mask)
+#define CHECKBIT(mask, bit) (mask & (1LL << bit))
+#define ONBIT(mask, bit) (mask | (1LL << bit))
+#define OFFBIT(mask, bit) (mask & ~(1LL << bit))
+#define CHANGEBIT(mask, bit) (mask ^ (1LL << bit))
+
+void solve()
+{
+    ll n, s, r;
+    cin >> n >> s >> r;
+    int re = r % (n - 1);
+    int val = r / (n - 1);
+    vll v(n - 1, val);
+    int i = 0;
+    while (re--)
+    {
+        if (v[i % (n - 1)] < 6)
+            v[i % (n - 1)]++;
+            i++;
+    }
+    f(i,0,n-1)
+    cout<<v[i]<<" ";
+    cout << (s - r) << endl;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0), cout.tie(0);
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+}
