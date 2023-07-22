91572197060f341c116f1ed698b8e35948e488e5
achyut vardhan <achyutvardhan234@gmail.com>
2023-07-22 22:34:10 +0530

    22/7/23

---
+++ codeforce/A_Vika_and_Her_Friends.cpp
@@ -0,0 +1,60 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+#define f(i, a, b) for (ll i = a; i < b; i++)
+#define r(i, a, b) for (ll i = a; i > b; i--)
+#define re(i, a, b) for (ll i = a; i >= b; i--)
+#define vsi vector<string, ll>
+#define vll vector<ll>
+#define mll map<ll, ll>
+#define pll pair<ll, ll>
+#define mp map<pll, ll>
+#define mi INT_MAX
+///.........Bit_Manipulation...........///
+#define bit (1ll << 35) - 1
+#define MSB(mask) 63 - __builtin_clzll(mask) /// 0 -> -1
+#define LSB(mask) __builtin_ctzll(mask)      /// 0 -> 64
+#define SETBIT(mask) __builtin_popcountll(mask)
+#define CHECKBIT(mask, bit) (mask & (1LL << bit))
+#define ONBIT(mask, bit) (mask | (1LL << bit))
+#define OFFBIT(mask, bit) (mask & ~(1LL << bit))
+#define CHANGEBIT(mask, bit) (mask ^ (1LL << bit))
+
+void solve()
+{
+    ll n, m, k;
+    cin >> n >> m >> k;
+    ll x, y;
+    cin >> x >> y;
+   vector<pll> v(k);
+    f(i, 0, k)
+            cin >>v[i].first>>v[i].second;
+    
+   f(i,0,k)
+   {
+     if((x+y)%2==0&&(v[i].first+v[i].second)%2==0)
+     {
+        cout<<"NO"<<endl;
+        return;
+     }
+     else if((x+y)%2!=0&&(v[i].first+v[i].second)%2!=0)
+     {
+        cout<<"NO"<<endl;
+        return;
+     }
+   }
+   cout<<"YES"<<endl;
+    
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0), cout.tie(0);
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+}
