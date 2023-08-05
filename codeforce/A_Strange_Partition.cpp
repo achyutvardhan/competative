e378d9a41d5d88b2bcd0e39942e4dc5895e05574
achyut vardhan <achyutvardhan234@gmail.com>
2023-08-05 15:19:11 +0530

    4/7/23

---
+++ codeforce/A_Strange_Partition.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int  
+#define f(i,a,b) for (ll i = a; i < b; i++)
+#define r(i,a,b) for (ll i = a; i > b; i--)
+#define re(i,a,b) for (ll i = a; i >= b; i--)
+#define vsi vector<string, ll >
+#define fibo(n) (pow(1.618,n)-pow((1-1.618),n))/sqrt(5)
+#define Acon(n) static_cast<char>(n)
+#define gcd(a,b) __gcd(a,b)
+#define lcm(a,b) (1LL*(a/gcd(a,b))*b)
+#define vll vector<ll>
+#define mll map<ll,ll>
+#define pll pair<ll,ll>
+#define mp map<pll,ll>
+#define mi  INT_MAX
+///.........Bit_Manipulation...........///
+#define bit (1ll<<35)-1
+#define MSB(mask) 63-__builtin_clzll(mask)  /// 0 -> -1
+#define LSB(mask) __builtin_ctzll(mask)  /// 0 -> 64
+#define SETBIT(mask) __builtin_popcountll(mask)
+#define CHECKBIT(mask,bit) (mask&(1LL<<bit))
+#define ONBIT(mask,bit) (mask|(1LL<<bit))
+#define OFFBIT(mask,bit) (mask&~(1LL<<bit))
+#define CHANGEBIT(mask,bit) (mask^(1LL<<bit))
+#define CeilDiv(a,b) ((a+b-1)/b)
+ 
+void solve(){
+    ll n,x;
+    cin>>n>>x;
+    vll v(n);
+    ll sum1 =0;
+    ll sum2 =0;
+    f(i,0,n)
+    {
+      cin>>v[i];
+      sum1+=v[i];
+      sum2+= ceil((double)v[i]/x);
+    }
+    cout<<(long long)ceil((double)sum1/x)<<" "<<(long long)sum2<<endl;
+}
+ 
+ 
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0),cout.tie(0);
+     int t;
+     cin>>t;
+  while(t--){
+     solve();
+    }
+}
---
+++ codeforce/B_Luntik_and_Subsequences.cpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int  
+#define f(i,a,b) for (ll i = a; i < b; i++)
+#define r(i,a,b) for (ll i = a; i > b; i--)
+#define re(i,a,b) for (ll i = a; i >= b; i--)
+#define vsi vector<string, ll >
+#define fibo(n) (pow(1.618,n)-pow((1-1.618),n))/sqrt(5)
+#define Acon(n) static_cast<char>(n)
+#define gcd(a,b) __gcd(a,b)
+#define lcm(a,b) (1LL*(a/gcd(a,b))*b)
+#define vll vector<ll>
+#define mll map<ll,ll>
+#define pll pair<ll,ll>
+#define mp map<pll,ll>
+#define mi  INT_MAX
+///.........Bit_Manipulation...........///
+#define bit (1ll<<35)-1
+#define MSB(mask) 63-__builtin_clzll(mask)  /// 0 -> -1
+#define LSB(mask) __builtin_ctzll(mask)  /// 0 -> 64
+#define SETBIT(mask) __builtin_popcountll(mask)
+#define CHECKBIT(mask,bit) (mask&(1LL<<bit))
+#define ONBIT(mask,bit) (mask|(1LL<<bit))
+#define OFFBIT(mask,bit) (mask&~(1LL<<bit))
+#define CHANGEBIT(mask,bit) (mask^(1LL<<bit))
+ 
+ 
+void solve(){
+    ll n;
+    cin>>n;
+    vll v(n);
+    ll cou0 =0;
+    ll cou1 = 0;
+    f(i,0,n){
+    cin>>v[i];
+    if(v[i]==0)cou0++;
+    if(v[i]==1)cou1++;
+    }
+   cout<<(ll)(pow(2,cou0)*cou1)<<endl;
+}
+ 
+ 
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0),cout.tie(0);
+     int t;
+     cin>>t;
+  while(t--){
+     solve();
+    }
+}
