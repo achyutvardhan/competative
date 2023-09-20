cd1fe760f8905626365f4767492b4e67a76d8f87
achyut vardhan <achyutvardhan234@gmail.com>
2023-09-20 12:37:26 +0530

    Create zero_sum.cpp

---
+++ zero_sum.cpp
@@ -0,0 +1,74 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+#define f(i, a, b) for (ll i = a; i < b; i++)
+#define r(i, a, b) for (ll i = a; i > b; i--)
+#define re(i, a, b) for (ll i = a; i >= b; i--)
+#define vsi vector<string, ll>
+#define fibo(n) (pow(1.618, n) - pow((1 - 1.618), n)) / sqrt(5)
+#define Acon(n) static_cast<char>(n)
+#define gcd(a, b) __gcd(a, b)
+#define lcm(a, b) (1LL * (a / gcd(a, b)) * b)
+#define vll vector<ll>
+#define mll map<ll, ll>
+#define pll pair<ll, ll>
+#define mp map<pll, ll>
+#define mi INT_MAX
+#define CeilDiv(a, b) ((a + b - 1) / b)
+///.........Bit_Manipulation...........///
+#define bit (1ll << 35) - 1
+#define MSB(mask) 63 - __builtin_clzll(mask) /// 0 -> -1
+#define LSB(mask) __builtin_ctzll(mask)      /// 0 -> 64
+#define SETBIT(mask) __builtin_popcountll(mask)
+#define CHECKBIT(mask, bit) (mask & (1LL << bit))
+#define ONBIT(mask, bit) (mask | (1LL << bit))
+#define OFFBIT(mask, bit) (mask & ~(1LL << bit))
+#define CHANGEBIT(mask, bit) (mask ^ (1LL << bit))
+
+void solve()
+{
+    ll n;
+    cin >> n;
+    // cout << 1 << endl;
+    vll v(n);
+    ll sum = 0;
+    f(i, 0, n)
+    {
+        cin >> v[i];
+        // cout << v[i] << endl;
+        sum += v[i];
+    }
+    if (sum == 0)
+    {
+        cout << "NO" << endl;
+        return;
+    }
+    cout << "YES" << endl;
+    ll c0 = count(v.begin(), v.end(), 0);
+    sort(v.begin(), v.end());
+    f(i, 0, n)
+    {
+        if (v[i] != 0)
+            cout
+                << v[i] << " ";
+    }
+
+    f(i, 0, c0)
+    {
+        cout
+            << 0 << " ";
+    }
+    cout << endl;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0), cout.tie(0);
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+}
