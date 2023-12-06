760695e5216b7ecf7f334d72b552d280dd95c6cd
achyut vardhan <achyutvardhan234@gmail.com>
2023-12-06 23:06:09 +0530

    6-12-23

---
+++ codeforce/B_YetnotherrokenKeoard.cpp
@@ -0,0 +1,83 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+#define f(i, a, b) for (ll i = a; i < b; i++)
+#define r(i, a, b) for (ll i = a; i > b; i--)
+#define re(i, a, b) for (ll i = a; i >= b; i--)
+#define vsi vector<string, ll>
+#define fibo(n) (pow(1.618, n) - pow((1 - 1.618), n)) / sqrt(5)
+#define Acon(n) static_cast<char>(n)
+#define gcd(a, b) __gcd(a, b)
+#define lcm(a, b) (1LL * (a / gcd(a, b)) * b)
+#define vll vector<ll>
+#define mll map<ll, ll>
+#define pll pair<ll, ll>
+#define mp map<pll, ll>
+#define mi INT_MAX
+#define CeilDiv(a, b) ((a + b - 1) / b)
+#define mod 1000000007
+///.........Bit_Manipulation...........///
+#define bit (1ll << 35) - 1
+#define MSB(mask) 63 - __builtin_clzll(mask) /// 0 -> -1
+#define LSB(mask) __builtin_ctzll(mask)      /// 0 -> 64
+#define SETBIT(mask) __builtin_popcountll(mask)
+#define CHECKBIT(mask, bit) (mask & (1LL << bit))
+#define ONBIT(mask, bit) (mask | (1LL << bit))
+#define OFFBIT(mask, bit) (mask & ~(1LL << bit))
+#define CHANGEBIT(mask, bit) (mask ^ (1LL << bit))
+
+void solve()
+{
+    // b - backespace(lowercase)
+    // B - backspace(uppercase)
+
+    string s;
+    cin >> s;
+    vector<int> lo_stack;
+    vector<int> up_stack;
+    f(i, 0, s.length())
+    {
+        if (s[i] != 'b' && s[i] != 'B')
+        {
+            if (s[i] >= 'a' && s[i] <= 'z')
+                lo_stack.push_back(i);
+            else
+                up_stack.push_back(i);
+        }
+        else
+        {
+            if (!lo_stack.empty()&&s[i] == 'b')
+            {
+                int x = lo_stack.back();
+                s[x] = '1';
+                lo_stack.pop_back();
+            }
+            else if(!up_stack.empty()&&s[i] == 'B')
+            {
+
+                int x = up_stack.back();
+                s[x] = '1';
+                up_stack.pop_back();
+            }
+        }
+    }
+
+    f(i, 0, s.length())
+    {
+        if (s[i] != '1' && s[i] != 'b' && s[i] != 'B')
+            cout << s[i];
+    }
+    cout << endl;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0), cout.tie(0);
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+}
